9748b79d80b3fbbf8e5ede23aed0973f56922eb3
Emil <emil@dubielecki.pl>
2022-03-08 10:00:05 +0100

    Add sidePanel with statuses

---
+++ src/files/client/custom/modules/dubas-shipping-manager/src/views/dubas-package/record/panels/packageStatuses.js
@@ -0,0 +1,6 @@
+Espo.define('dubas-shipping-manager:views/dubas-package/record/panels/packageStatuses', 'views/record/panels/relationship', function (Dep) {
+
+    return Dep.extend({
+        
+    });
+});
