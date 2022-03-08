501bb839912b583b911b99b4d182836341f808d8
Emil <emil@dubielecki.pl>
2022-03-08 10:01:33 +0100

    ecs

--- src/files/custom/Espo/Modules/DubasShippingManager/Hooks/DubasPackage/GeneratePackageNumber.php
+++ src/files/custom/Espo/Modules/DubasShippingManager/Hooks/DubasPackage/GeneratePackageNumber.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * This file is part of the Dubas Shipping Manager - EspoCRM extension.
+ *
+ * DUBAS S.C. - contact@dubas.pro
+ * Copyright (C) 2022 Arkadiy Asuratov, Emil Dubielecki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace Espo\Modules\DubasShippingManager\Hooks\DubasPackage;
 
 use Espo\ORM\Entity;
@@ -16,16 +36,15 @@ class GeneratePackageNumber
 
     public function beforeSave(Entity $entity, array $options, array $data): void
     {
-        if($entity->isNew() && !$entity->get('name')){
-
+        if ($entity->isNew() && !$entity->get('name')) {
             $amount = $this->entityManager
-            ->getRDBRepository('DubasPackage')
-            ->where([
-                'parcelId' => $entity->get('parcelId')
-            ])
-            ->count();
+                ->getRDBRepository('DubasPackage')
+                ->where([
+                    'parcelId' => $entity->get('parcelId'),
+                ])
+                ->count();
 
-            $entity->set('name', $entity->get('parcelName').sprintf("%'.03d\n", $amount+1));
+            $entity->set('name', $entity->get('parcelName') . sprintf("%'.03d\n", $amount + 1));
         }
     }
 }
